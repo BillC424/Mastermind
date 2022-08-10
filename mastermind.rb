c3719874351f24fe0b987ca7a394f855e993bfab
BillC424 <wcurran44@gmail.com>
2022-08-10 11:42:48 -0400

    Modified CodeMaker class to add a method to allow for human player to modify the secret-colors instance variable. Changed play_game method to direct to that CodeMaker method at the beginning of the game if they choose to be the CodeMaker

--- mastermind.rb
+++ mastermind.rb
@@ -1,17 +1,21 @@
-class Player
+class CodeBreaker
 
 end
 
-class CodeMaker < Player
+class CodeMaker 
   attr_accessor :secret_colors
   
   def initialize
     @secret_colors = ['blue', 'yellow', 'red', 'green', 'orange', 'purple'].sample(4)
   end
-end
-
-class CodeBreaker < Player
-
+  def human_secret_colors
+    @secret_colors = Array.new
+    4.times do |index|
+    puts "What color do you want to choose for peg #{index + 1}?"
+     peg_choice = gets.chomp.downcase
+     @secret_colors.push(peg_choice)
+    end
+  end
 end
 
 class Board
@@ -21,37 +25,45 @@ class Board
   end
     
   def play_game(codeMaker)
-    12.times do  
-      correct_color_space = 0 
-      correct_color = 0
-      if @game == 'won'
-        break
-      end
-      4.times do
-        codeMaker.secret_colors.each_with_index do |secret_color, index|
-          puts "What color do you want to choose for peg #{index + 1}?"
-          choice = gets.chomp.downcase
-          if choice == secret_color
-            correct_color_space += 1
-          else 
-            codeMaker.secret_colors.each do |secret_color|
-              if choice == secret_color
-                correct_color += 1
+    puts "Would you like to be the codebreaker or the codemaker?"
+    code_choice = gets.chomp.downcase
+    if code_choice != "codemaker" && code_choice != "codebreaker"
+      puts  "That's not one of the choices."
+      play_game(codeMaker)
+    elsif code_choice == "codemaker"
+      codeMaker.human_secret_colors
+    else 
+      12.times do  
+        correct_color_space = 0 
+        correct_color = 0
+        if @game == 'won'
+          break
+        end
+        4.times do
+          codeMaker.secret_colors.each_with_index do |secret_color, index|
+            puts "What color do you want to choose for peg #{index + 1}?"
+            choice = gets.chomp.downcase
+            if choice == secret_color
+              correct_color_space += 1
+            else 
+              codeMaker.secret_colors.each do |secret_color|
+                if choice == secret_color
+                  correct_color += 1
+                end
               end
             end
           end
-        end
-       if correct_color_space == 4
-         @game = 'won'
-         puts 'You win!'
-         break
-       else  
-       puts "There were #{correct_color_space} choices with the correct color and correct space selected. #{correct_color} choices had the correct color, but the incorrect space was picked"
-       p codeMaker.secret_colors
-       correct_color_space = 0 
-       correct_color = 0
-       end
-      end  
+         if correct_color_space == 4
+           @game = 'won'
+           puts 'You win!'
+           break
+         else  
+         puts "There were #{correct_color_space} choices with the correct color and correct space selected. #{correct_color} choices had the correct color, but the incorrect space was picked"
+         correct_color_space = 0 
+         correct_color = 0
+         end
+        end  
+      end
     end
   end
 end
